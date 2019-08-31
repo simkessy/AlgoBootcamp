f443dd503720a5c93e5b5868014a7fb1a11faaee
Kessy Similien <simkessy@gmail.com>
2019-08-31 13:35:17 -0700

    Solve Reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,42 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+/* 
+function reverse(str) {
+  return str
+    .split("")
+    .reverse()
+    .join("");
+} 
+*/
+
+/* 
+Use a loop an push to a new array backwards
+Don't forget arr.length-1
+function reverse(str) {
+  let arr = [];
+  for (let i = str.length - 1; i >= 0; i--) {
+    arr.push(str[i]);
+  }
+  return arr.join("");
+} 
+*/
+
+/* 
+function reverse(str) {
+  let reversed = "";
+
+  for (let ltr of str) {
+    //   Keep adding the last letter to the front
+    reversed = ltr + reversed;
+  }
+
+  return reversed;
+} 
+*/
+
+function reverse(str) {
+  return str.split("").reduce((rev, char) => char + rev, "");
+}
 
 module.exports = reverse;
