4f3f60eba65c4d09588d45754f5aaf7e58cf9846
Kessy Similien <simkessy@gmail.com>
2019-09-01 13:33:22 -0700

    Implement Spiral matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,54 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let results = [];
 
+  // Build result structure
+  for (let i = 0; i < n; i++) {
+    results.push([]);
+  }
+
+  let start_col = 0;
+  let end_col = n - 1;
+  let start_row = 0;
+  let end_row = n - 1;
+
+  let counter = 1;
+
+  while (start_col <= end_col && start_row <= end_row) {
+    console.log("start_col:", start_col);
+    // responsible for top row
+    for (let i = start_col; i <= end_col; i++) {
+      results[start_row][i] = counter;
+      counter++;
+    }
+    start_row++;
+
+    // Right column
+    for (let i = start_row; i <= end_row; i++) {
+      results[i][end_col] = counter;
+      counter++;
+    }
+    end_col--;
+
+    // Bottom row
+    for (let i = end_col; i >= start_col; i--) {
+      results[end_row][i] = counter;
+      counter++;
+    }
+    end_row--;
+
+    // start column
+    for (let i = end_row; i >= start_row; i--) {
+      results[i][start_col] = counter;
+      counter++;
+    }
+    start_col++;
+  }
+
+  console.log("results:", results);
+  return results;
+}
+matrix(2);
 module.exports = matrix;
