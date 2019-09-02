f3c3e08689f62bcaeb9df28069178ef1c50d37bd
Kessy Similien <simkessy@gmail.com>
2019-09-02 12:16:50 -0700

    Implement BST validation

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,21 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  if (max !== null && node.data > max) return false;
+  if (min !== null && node.data < min) return false;
+
+  // Check if there's a left node
+  // Check if left node is less than parent
+  // if not - check again but up update max
+  if (node.left && !validate(node.left, min, node.data)) return false;
+
+  // Check if there's a right node
+  // Check if right node is less than parent
+  // if not - check again but up update min
+  if (node.right && !validate(node.right, node.data, max)) return false;
+
+  return true;
+}
 
 module.exports = validate;
